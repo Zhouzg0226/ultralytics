51c537ec4bd603cfe07c88fdbc4211256301cf61
Zhouzg0226 <Zhouzg0226@163.com>
2025-02-27 18:18:34 +0800

    复制官方的修复【RBOX 正则化：针对 OpenCV 定义的一致性，对边界框角度计算进行优化。】

--- ultralytics/utils/ops.py
+++ ultralytics/utils/ops.py
@@ -800,9 +800,13 @@ def regularize_rboxes(rboxes):
     """
     x, y, w, h, t = rboxes.unbind(dim=-1)
     # Swap edge and angle if h >= w
-    w_ = torch.where(w > h, w, h)
-    h_ = torch.where(w > h, h, w)
-    t = torch.where(w > h, t, t + math.pi / 2) % math.pi
+    # w_ = torch.where(w > h, w, h)
+    # h_ = torch.where(w > h, h, w)
+    # t = torch.where(w > h, t, t + math.pi / 2) % math.pi
+    swap = t % math.pi >= math.pi / 2
+    w_ = torch.where(swap, h, w)
+    h_ = torch.where(swap, w, h)
+    t = t % (math.pi / 2)
     return torch.stack([x, y, w_, h_, t], dim=-1)  # regularized boxes
 
 
